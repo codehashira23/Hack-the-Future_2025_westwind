96b2f7be8f1a79830bf13b02671f1b34e0d783b3
bala subramanian <skv.balasubramanian@gmail.com>
2025-03-29 01:31:05 +0530

    login partial

--- alumix/src/login/login.js
+++ alumix/src/login/login.js
@@ -6,6 +6,30 @@ function SignInForm() {
     });
     const handlechange = evt => {
         const value= evt.target.value;
-        setState
-    }
+        setState({
+            ...state,
+            [evt.target.value]:value
+        });
+    };
+    const handleOnSubmit = evt =>{
+        evt.preventDefault();
+        const {email,password}= state;
+        alert('you have logged in with email: ${email} and ${password}')
+    for (const key in state) {
+        setState({
+          ...state,
+          [key]: ""
+        });
+      }
+    };
+    return (
+        <div>
+            <form onSubmit={handleOnSubmit}>
+                <H1>Sign In</H1>
+                <div>
+                    
+                </div>
+            </form>
+        </div>
+    )
 }
