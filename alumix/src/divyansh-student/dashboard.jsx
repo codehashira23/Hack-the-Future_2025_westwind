a3eba822b49fd94bf5e9f044cefcc81aec54d7f3
Divyansh <divyanshjaiswal314@gmail.com>
2025-03-29 00:08:01 +0530

    student dash

---
+++ alumix/src/divyansh-student/dashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./dashboard.css";
+
+const StudentDashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Student Dashboard</h1>
+      <div className="dashboard-links">
+        <Link to="/student/mentorship">Find a Mentor</Link>
+        <Link to="/student/jobs">Job Board</Link>
+        <Link to="/student/events">Events</Link>
+        <Link to="/student/discussions">Discussions</Link>
+      </div>
+    </div>
+  );
+};
+
+export default StudentDashboard;
