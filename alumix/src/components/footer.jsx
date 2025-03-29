db590c12c11f9d5b4d3c6bc93cdc5773107d9346
codehashira23 <divyanshjaiswal314@gmail.com>
2025-03-29 11:47:17 +0530

    ui changes

--- alumix/src/components/footer.jsx
+++ alumix/src/components/footer.jsx
@@ -4,7 +4,7 @@ import "../styles/footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 ALUMIX. All rights reserved.</p>
+      <p>© 2025 ALUMiNix. All rights reserved.</p>
     </footer>
   );
 };
--- alumix/src/divyansh-student/dashboard.jsx
+++ alumix/src/divyansh-student/dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "../components/navbar";
 import { Link } from "react-router-dom";
 import "./dashboard.css";
 
 const StudentDashboard = () => {
   return (
     <div className="dashboard-container">
+      <Navbar></Navbar>  
       <h1>Student Dashboard</h1>
       <div className="dashboard-links">
         <Link to="/student/mentorship">Find a Mentor</Link>
