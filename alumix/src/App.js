05f87e55c27b477f34acc9cc93acf7b73fcffc52
codehashira23 <divyanshjaiswal314@gmail.com>
2025-03-29 20:45:44 +0530

    student dashboard and sidebar

--- alumix/src/App.js
+++ alumix/src/App.js
@@ -4,6 +4,8 @@ import Home from "../src/Home";
 import SignInForm from "./login/login.js";
 import SignUp from "./login/Signup";
 // import Login from "./pages/Login";
+import StudentDashboard from "./divyansh-student/dashboard.jsx";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         {/* <Route path="/login/:role" element={<Login />} /> */}
         <Route path="/login" element={<SignInForm />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/student/dashboard" element={<StudentDashboard />} />
 
       </Routes>
     </Router>
--- alumix/src/divyansh-student/dashboard.jsx
+++ alumix/src/divyansh-student/dashboard.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import Navbar from "../components/navbar";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Sidebar from "../components/student-components/sidebar";
 import "./dashboard.css";
+import { Posts, ExploreMentor, Profile } from "./sections"; // Import section components
 
 const StudentDashboard = () => {
+  const [activeSection, setActiveSection] = useState("Posts"); // Default section
+
   return (
-    <div className="dashboard-container">
-      <Navbar></Navbar>  
-      <h1>Student Dashboard</h1>
-      <div className="dashboard-links">
-        <Link to="/student/mentorship">Find a Mentor</Link>
-        <Link to="/student/jobs">Job Board</Link>
-        <Link to="/student/events">Events</Link>
-        <Link to="/student/discussions">Discussions</Link>
+    <div id="dashboard-container">
+      <div id="dashboard-flex">
+        <Sidebar setActiveSection={setActiveSection} />
+        <div id="dashboard-main">
+          <h1>Stay Organized, Stay Productive</h1>
+
+          <div className="dashboard-sections">
+            {activeSection === "Posts" && <Posts />}
+            {activeSection === "ExploreMentor" && <ExploreMentor />}
+            {activeSection === "Profile" && <Profile />}
+          </div>
+        </div>
       </div>
     </div>
   );
